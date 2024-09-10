41c42a196c5641400281bee9f95254c589c05227
wandl <hiwepy@163.com>
2024-09-10 15:33:02 +0800

    2.3.x.20240823-SNAPSHOT

--- src/main/java/org/apache/axis2/spring/boot/Axis2Properties.java
+++ src/main/java/org/apache/axis2/spring/boot/Axis2Properties.java
@@ -5,7 +5,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(Axis2Properties.PREFIX)
 public class Axis2Properties {
 
-	public static final String PREFIX = "spring.disruptor";
- 
+	public static final String PREFIX = "spring.axis2";
+
+	/**
+	 * Enable Axis2.
+	 */
+	private boolean enabled = true;
+
+
 	
 }
